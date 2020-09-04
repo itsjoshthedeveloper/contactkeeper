6637a6ca075aead8e6a994b28e38f2e5adeb59bd
Josh Chough <joshchough@gmail.com>
2020-09-03 21:34:15 -0400

    Contact Model & Get Contacts Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,30 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route     GET api/contacts
 // @desc      Get all user's contacts
-// @access    Public
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+// @access    Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route     POST api/contacts
 // @desc      Add new contacts
-// @access    private
+// @access    Private
 router.post('/', (req, res) => {
   res.send('Add contact');
 });
 
 // @route     PUT api/contacts/:id
 // @desc      Update contact
-// @access    private
+// @access    Private
 router.put('/:id', (req, res) => {
   res.send('Update contact');
 });
 
 // @route     DELETE api/contacts/:id
 // @desc      Delete contact
-// @access    private
+// @access    Private
 router.delete('/:id', (req, res) => {
   res.send('Delete contact');
 });
