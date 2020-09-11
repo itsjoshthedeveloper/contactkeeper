48926a24097736a5d4a5a017dde764bc3516a72b
Josh Chough <joshchough@gmail.com>
2020-09-10 22:09:46 -0400

    Set & Clear Current Contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,6 +37,7 @@ const ContactState = (props) => {
         type: 'personal',
       },
     ],
+    current: null,
   };
 
   const [state, dispatch] = useReducer(ContactReducer, initialState);
@@ -53,8 +54,14 @@ const ContactState = (props) => {
   };
 
   // Set Current Contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
 
   // Clear Current Contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
 
   // Update Contat
 
@@ -66,8 +73,11 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
         deleteContact,
+        setCurrent,
+        clearCurrent,
       }}
     >
       {props.children}
